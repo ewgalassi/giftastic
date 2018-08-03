c4c73e33b0700dd0ec437d2e3beafadb9d8d6584
Muscle Everest <danza_seppia@yahoo.com>
2018-08-03 14:23:46 -0500

    script additions

--- assets/script.js
+++ assets/script.js
@@ -3,7 +3,22 @@ $(document).ready(function() {
 
     function renderButtons() {
         for (item in topics) {
-
+            var btn = $("<button>");
+            btn.attr("data-name", topics[item]);
+            btn.text(topics[item]);
+            $("#buttons-view").append(btn);
         }
-    }
+    };
+
+    function displayGifs() {
+        var subject = $(this).attr("data-name");
+        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + subject + "&api_key=YOUR_API_KEY&limit=5"
+    };
+
+    $("#add-meme").click(function(event) {
+        event.preventDefault();
+        var meme = $("#meme-input").val().trim();
+        topics.push(meme);
+        renderButtons();
+    });
 })

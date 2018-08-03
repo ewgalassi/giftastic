dffcabdf4069b828e91d7fa3d89ef471530eccb7
Muscle Everest <danza_seppia@yahoo.com>
2018-08-03 13:54:19 -0500

    changes

---
+++ assets/script.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+    var topics = ["doge", "not my concern", "y u no", "not sure if", "all the"]
+
+    function renderButtons() {
+        for (item in topics) {
+
+        }
+    }
+})

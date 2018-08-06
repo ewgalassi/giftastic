ccfa0e27bf4b0d6fab9d19bc2c6b225c01bc85a4
Muscle Everest <danza_seppia@yahoo.com>
2018-08-05 20:50:13 -0500

    final fixes

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-    var topics = ["doge", "not my concern", "y u no", "not sure if", "all the"];
+    var topics = ["doge", "but that's none of my business", "y u no", "futurama fry", "all the things"];
 
     function renderButtons() {
         $("#buttons-view").empty();
@@ -13,6 +13,7 @@ $(document).ready(function() {
     };
 
     function displayGifs() {
+        $("#memes-view").empty();
         var subject = $(this).attr("data-name");
         var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + subject + "&api_key=JaYZ7DBSrMFf0TxZxFLnXRhOQ8x12zyW&limit=10";
 
@@ -20,15 +21,17 @@ $(document).ready(function() {
             url: queryURL,
             method: "GET"
         }).then(function(response) {
-            console.log(response);
             var results = response.data;
             for (item in results) {
-                var gifDiv = $("<div data-motion='still'>");
+                var gifDiv = $("<div>");
                 var rating = results[item].rating;
                 var p = $("<p>").text("rating: " + rating);
                 var memeImage = $("<img>");
-                memeImage.attr("src", results[item].images.fixed_height_still.url)
-                memeImage.attr("animated", results[item].images.fixed_height.url);
+                memeImage.attr("data-state", "still");
+                memeImage.attr("class", "gif");
+                memeImage.attr("src", results[item].images.fixed_height_still.url);
+                memeImage.attr("data-still", results[item].images.fixed_height_still.url);
+                memeImage.attr("data-animate", results[item].images.fixed_height.url);
                 gifDiv.prepend(p);
                 gifDiv.prepend(memeImage);
                 $("#memes-view").prepend(gifDiv);
@@ -36,11 +39,22 @@ $(document).ready(function() {
         });
     };
 
-    $("#add-meme").click(function(event) {
+    $("#add-meme").on("click", function(event) {
         event.preventDefault();
         var meme = $("#meme-input").val().trim();
         topics.push(meme);
         renderButtons();
+    });
+
+    $(document).on("click", ".gif", function() {
+        var state = $(this).attr("data-state");
+        if (state === "still") {
+            $(this).attr("src", $(this).attr("data-animate"));
+            $(this).attr("data-state", "animate");
+        } else if (state === "animate") {
+            $(this).attr("src", $(this).attr("data-still"));
+            $(this).attr("data-state", "still");
+        }
     });
 
     $(document).on("click", ".meme", displayGifs);
